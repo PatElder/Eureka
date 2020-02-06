06d4a5640aece1f4224225467fb90ab533cbe86e
Patrick Elder <patelder95@gmail.com>
2020-02-06 14:06:44 -0500

    API: Receiving JSON with articles!

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,10 @@
 import _ from "lodash";
 import "../css/style.css";
-import Icon from "../icon.jpg";
+import { getNews } from "../js/newsFetch";
 
 function component() {
   const element = document.createElement("div");
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
+  const news = getNews("Trump", 2, "us").then(data => console.log(data));
   return element;
 }
 
