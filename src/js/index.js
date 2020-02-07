c06b4dad967a72786c42669b1e084f80443c76f6
Patrick Elder <patelder95@gmail.com>
2020-02-07 13:44:51 -0500

    API: Results visible on the page :D

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
-import _ from "lodash";
+import newsTemplate from "../hbs/news.hbs";
 import "../css/style.css";
 import { getNews } from "../js/newsFetch";
 
-function component() {
-  const element = document.createElement("div");
-  const news = getNews("Trump", 2, "us").then(data => console.log(data));
-  return element;
+async function data() {
+  const newsData = await getNews("Trump", 5, "us");
+  for (let i = 0; i < newsData.totalResults - 1; i++) {
+    const newsEl = document.createElement("div");
+    newsEl.innerHTML = `<div><img src=${newsData.articles[i].urlToImage} /><div class="news">${newsData.articles[i].title}</div></div>`;
+    console.log(newsData.articles[i]);
+    document.body.appendChild(newsEl);
+  }
+  return newsData;
 }
 
-document.body.appendChild(component());
+window.onload = () => {
+  data();
+};
