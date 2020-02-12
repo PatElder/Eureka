98ef853a9163b9ade8a74a2680b5cdb5c177cc8d
gracia <graxxia@hotmail.com>
2020-02-12 10:25:06 -0500

    Auto stash before merge of "styling" and "origin/LStorage"

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
 import "../css/style.css";
+  const imgNav = document.createElement("div");
+  imgNav.classList.add("logo");
+  header.appendChild(imgNav);
 import { createHeadings } from "./components/menu";
 window.onload = async () => {
   await createHeadings();
--- src/js/newsFetch.js
+++ src/js/newsFetch.js
@@ -1,5 +1,5 @@
 export const getNews = async (search, num, language) => {
-  const key = "6eff9ed09ee54486b564c773b6db91b3";
+  const key = "4740308a03e949d280f53b3c4da9da17";
   const url = `https://newsapi.org/v2/everything?q=${search}&language=${language}&pageSize=${num}&apiKey=${key}`;
   const newsResponse = await fetch(url);
   const newsData = await newsResponse.json();
