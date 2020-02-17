030da58588dba23e6a24bcacea18f1124cd4047e
gracia <graxxia@hotmail.com>
2020-02-16 21:58:17 -0500

    index.js deleted function, style fixed
    deleted function dropdown index.js, fixed list menu
    resend

--- src/js/index.js
+++ src/js/index.js
@@ -8,16 +8,3 @@ window.onload = async () => {
   createHome();
   createFooter();
 };
-
-window.onclick = function(event) {
-  if (!event.target.matches(".dropbtn")) {
-    const dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
